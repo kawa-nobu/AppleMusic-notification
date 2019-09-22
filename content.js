5ce9290378dd8b809037d67d831b30871aa3a518
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2019-09-23 01:36:41 +0900

    エラー解消
    無駄に気持ち悪いエラーが出る問題を解消しました。

--- content.js
+++ content.js
@@ -1,25 +1,24 @@
 setInterval(appl, 100);
 function appl() {
 
-if (document.getElementById('apple-music-player') ==! null) {
+if (document.getElementsByClassName('web-chrome-playback-lcd__song-name-scroll')[0] == null) {
 var test = true;
 }else{
-var ap_title = document.getElementsByClassName('web-chrome-playback-lcd__song-name-scroll')[0].innerText;
-var ap_al = document.getElementsByClassName('web-chrome-playback-lcd__sub-copy-scroll')[0].innerText;
-var artw = document.getElementsByClassName('media-artwork-v2__image')[0].currentSrc;
-var ar_rep = artw.replace('44x44bb', '540x540bb');
-		if ('mediaSession' in navigator) {
-			navigator.mediaSession.metadata = new MediaMetadata({
-				title: ap_title,
-				artist: ap_al,
-				album: "Test",
-				artwork: [{ src: ar_rep,  sizes: '540x540',   type: 'image/jpeg' }]
-			});
-			navigator.mediaSession.setActionHandler('play', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[1].click()});
-			navigator.mediaSession.setActionHandler('pause', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[1].click()});
-			navigator.mediaSession.setActionHandler('previoustrack', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[0].click()});
-			navigator.mediaSession.setActionHandler('nexttrack', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[2].click()});
-		}
+	var ap_title = document.getElementsByClassName('web-chrome-playback-lcd__song-name-scroll')[0].innerText;
+	var ap_al = document.getElementsByClassName('web-chrome-playback-lcd__sub-copy-scroll')[0].innerText;
+	var artw = document.getElementsByClassName('media-artwork-v2__image')[0].currentSrc;
+	var ar_rep = artw.replace('44x44bb', '540x540bb');
+	if ('mediaSession' in navigator) {
+		navigator.mediaSession.metadata = new MediaMetadata({
+			title: ap_title,
+			artist: ap_al,
+			album: "Test",
+			artwork: [{ src: ar_rep,  sizes: '540x540',   type: 'image/jpeg' }]
+		});
+		navigator.mediaSession.setActionHandler('play', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[1].click()});
+		navigator.mediaSession.setActionHandler('pause', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[1].click()});
+		navigator.mediaSession.setActionHandler('previoustrack', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[0].click()});
+		navigator.mediaSession.setActionHandler('nexttrack', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[2].click()});
 	}
 }
-
+}
