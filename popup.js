d03eb4bfb6659dded5c70d8b918a123b1bcf2ee2
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2019-10-27 12:27:51 +0900

    アニソン向け最適化&アイコン追加

--- popup.js
+++ popup.js
@@ -2,11 +2,13 @@ setInterval(send_data, 100);
 var artist_name;
 var song_name;
 var album_name;
+var artist_rep
 function send_data(){
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	song_name = message.songname;
 	artist_name = message.artist.substring(0, message.artist.indexOf(" —"));
 	album_name = message.artist.substring(100, message.artist.indexOf("— ")+2);
+	artist_rep = message.artist.substring(0, message.artist.indexOf(" —")).replace(/CV:/g, '');
 	document.getElementById('cover').src = message.img_url;
 	document.getElementById('songname').innerText = song_name;
 	document.getElementById('artist').innerText = artist_name;
@@ -19,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	var goto = document.getElementById('cover');
 	goto.addEventListener('click', function() {
 		chrome.windows.create({
-			url: "https://utaten.com/lyric/"+artist_name.trim()+"/"+song_name.trim()+"/",
+			url: "https://utaten.com/lyric/"+artist_rep.trim()+"/"+song_name.trim()+"/",
 			type: "popup",
 			height : 600,
 			width : 800
