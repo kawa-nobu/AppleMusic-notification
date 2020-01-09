5f207fc10182c603f67dacebc5ff003197dcbb1b
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2020-01-09 23:18:02 +0900

    Chrome79完全対応

--- content.js
+++ content.js
@@ -7,11 +7,12 @@ function appl() {
 		var ap_al = document.getElementsByClassName('web-chrome-playback-lcd__sub-copy-scroll')[0].innerText;
 		var artw = document.getElementsByClassName('media-artwork-v2__image')[0].currentSrc;
 		var ar_rep = artw.replace('44x44bb', '540x540bb');
+		var alb = document.getElementsByClassName('web-chrome-playback-lcd__sub-copy-scroll-inner-text-wrapper')[0].childNodes[3].textContent;
 		if ('mediaSession' in navigator) {
 			navigator.mediaSession.metadata = new MediaMetadata({
 				title: ap_title,
 				artist: ap_al,
-				album: "Test",
+				album: alb,
 				artwork: [{ src: ar_rep,  sizes: '540x540',   type: 'image/jpeg' }]
 			});
 			navigator.mediaSession.setActionHandler('play', function() {document.getElementsByClassName('button-reset web-chrome-playback-controls__playback-btn')[1].click()});
